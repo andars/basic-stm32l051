9eb9ec12414a2d32d3b2efc9df5b8c3cd3ad12e3
andrew <afoote97@gmail.com>
2023-10-15 14:16:51 -0700

    initialize GPIO port A

--- main.c
+++ main.c
@@ -1,4 +1,22 @@
+#include <stdint.h>
+
+#define RCC_BASE 0x40021000
+#define RCC_IOPENR 0x2C
+
+void gpio_init() {
+    // Reset & clock control, GPIO clock enable register
+    volatile uint32_t *rcc_iopenr = (volatile uint32_t *)(RCC_BASE + RCC_IOPENR);
+
+    // Enable GPIO port A clock
+    *rcc_iopenr = 0x1;
+}
+
+int main() {
+    gpio_init();
+}
+
 void reset_handler(void) {
+    main();
 }
 
 extern char stack_start[];
